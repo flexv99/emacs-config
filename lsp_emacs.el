3aefaffeed3300df0168dda40602f772ef5d0e6e
flex99 <felix@vertical-life.info>
2024-02-12 15:50:03 +0100

    new work config

--- lsp_emacs.el
+++ lsp_emacs.el
@@ -9,15 +9,8 @@
 ;;                                          |___/
 ;; Felix Valentini
 ;; Emcas configuration v.2
-;; last update 22.09.2020
+;; last update 2024-02-12
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(restclient dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company lsp-ui company hover)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,38 +18,45 @@
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
-
-;; start package.el with Emacs
+;; start package.el eith Emacs
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-;; add exec-path for external programs
-(add-to-list 'exec-path "/home/flex/.local/bin")
 ;; initialize package.el
 (package-initialize)
-
-;; space instead of tab
-(setq-default indent-tabs-mode nil)
-
 (setq inhibit-startup-screen t)
 ;; Disable tool-bar
 (tool-bar-mode -1)
-;; comment/uncomment bindings
+;; enable whitespace-mode
+;; (require 'whitespace)
+;; (global-whitespace-mode 1)
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(delete-selection-mode 1)
 (global-set-key (kbd "C-;") 'comment-or-uncomment-region)
-;; Font
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono" ))
-(set-face-attribute 'default t :font "DejaVu Sans Mono" )
+;; save annoying backupfiles outside of the working directories
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+(add-to-list 'image-types 'svg)
+;; use spaces instead of tab, to convert tabbed document to spaced one selct the whole doc (C-x h; M-x unatbify)
+(setq-default indent-tabs-mode nil)
 ;; Editor plug ins
-(use-package disable-mouse
-  :ensure t
-  :config
-  (require 'disable-mouse)
-  (global-disable-mouse-mode))
-
 (use-package counsel
   :ensure t)
-
 (use-package swiper
   :ensure t
   :config
@@ -82,7 +82,6 @@
     (global-set-key (kbd "C-x l") 'counsel-locate)
     (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
     (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)))
-
 ;; theme
 (use-package doom-themes
   :ensure t
@@ -91,29 +90,27 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-one t)
+
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; or for treemacs users
   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
+  
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
-
 ;; M-x all-the-icons-fonts to install them!
 (use-package all-the-icons
   :ensure t)
-
 ;; disable scroll-bar
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
-
 ;; Mod line configuration, used package: spaceline
 (use-package spaceline
   :ensure t
   :config
   (require 'spaceline-config)
   (spaceline-emacs-theme))
-
 ;; nyan-mode for mod line
 (use-package nyan-mode
   :ensure t
@@ -128,7 +125,6 @@
   (global-set-key (kbd "C-M-=") 'default-text-scale-increase)
   (global-set-key (kbd "C-M--") 'default-text-scale-decrease))
 (define-key ctl-x-map [(control ?0)] 'zoom-in/out)
-
 ;; line numeration
 (use-package linum-relative
   :ensure t
@@ -145,19 +141,11 @@
     (global-set-key [remap other-window] 'ace-window)
     (custom-set-faces
      '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0)))))121))
+       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 121))
 
 (use-package highlight-numbers
   :ensure t
   :hook (prog-mode-hook . highlight-numbers-mode))
-
-(use-package flycheck
-  :ensure t
-  ;; :init
-  ;; (global-flycheck-mode)
-  :config
-  (add-hook 'after-init-hook 'global-company-mode)
-  (global-flycheck-mode))
 
 (use-package emojify
   :ensure t
@@ -175,27 +163,20 @@
   :ensure t
   :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package rainbow-delimiters
+;; shows directories
+(use-package neotree
   :ensure t
-  :hook (progn-mode))
+  :config
+  (require 'neotree)
+  (global-set-key [f8] 'neotree-toggle)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+(setq-default indent-tabs-mode nil)
 
 ;; shows bindings options
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
-
-;; shows typed binding
-(use-package keycast
-  :config
-  (define-minor-mode keycast-mode
-    "Show current command and its key binding in the mode line."
-    :global t
-    (if keycast-mode
-	(add-hook 'pre-command-hook 'keycast--update t)
-      (remove-hook 'pre-command-hook 'keycast--update)))
-  (add-to-list 'global-mode-string '("" mode-line-keycast " "))
-  (keycast-mode))
 
 (use-package multiple-cursors
   :ensure t
@@ -206,23 +187,39 @@
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
 
+;; Programming plug-ins
 ;; git interface
 (use-package magit
-  :ensure t
-  :init
-  (progn
-    (bind-key "C-x g" 'magit-status)))
-
+  :ensure t)
 ;; project manager
 (use-package projectile
   :ensure t
   :config
-  ;; (projectile-mode +1)
+  (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (setq projectile-project-search-path '("~/work/vertical-life/02_src/")))
+  (define-key projectile-mode-map (kbd "C-c f") 'projectile-find-file))
 
-;; template system
+;; syntax checker
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t)
+  :config
+  ;; customize flycheck temp file prefix
+  (setq-default flycheck-temp-prefix ".flycheck")
+  ;; disable json-jsonlist checking for json files
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(json-jsonlist))))
+
+(use-package highlight-indentation
+  :ensure t
+  :config
+  (set-face-background 'highlight-indentation-face "#536878")
+  (set-face-background 'highlight-indentation-current-column-face "#657E91"))
+
+;; synatx templates
 (use-package yasnippet
   :ensure t
   :init
@@ -231,35 +228,21 @@
 (use-package yasnippet-snippets
   :ensure t)
 
-;; completion setup
-(use-package company
+;; lisp flavoured python
+(use-package hy-mode
   :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 3)
-  (add-hook 'after-init-hook 'global-company-mode)
-  (global-company-mode t))
+  :mode "\\.hy\\'")
 
-(use-package highlight-indentation
+;; javascript stuff
+;; rest api tester
+(use-package restclient
   :ensure t
-  :hook
-  (python-mode . highlight-indentation-mode)
-  (yaml-mode . highlight-indentation-mode)
-  :config
-  (set-face-background 'highlight-indentation-face "#536878")
-  (set-face-background 'highlight-indentation-current-column-face "#657E91"))
+  :mode (("\\.http\\'" . restclient-mode)))
 
-(use-package lsp-mode
+(use-package prettier-js
   :ensure t
-  :commands lsp
-  :custom
-  (lsp-auto-guess-root nil)
-  (lsp-prefer-flymake nil) ;; flycheck instead
-  :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
-  :hook ((python-mode c-mode dart-mode js-mode) . lsp))
-
-(use-package lsp-ui
-  :ensure t)
+  :after (rjsx-mode)
+  :hook (rjsx-mode . prettier-js-mode))
 
 ;; json utilities
 (use-package json-snatcher
@@ -267,19 +250,9 @@
   :config
   (require 'json-snatcher))
 
-;; rest api tester
-(use-package restclient
-  :ensure t
-  :mode (("\\.http\\'" . restclient-mode)))
-
 (use-package json-mode
   :ensure t
   :mode "\\.json\\'")
-
-(use-package prettier-js
-  :ensure t
-  :after (js-mode)
-  :hook (js-mode . prettier-js-mode))
 
 ;; yaml utilities
 (use-package yaml-mode
@@ -288,34 +261,62 @@
   :config
   (add-hook 'yaml-mode-hook 'highlight-indentation-mode))
 
-(use-package pyvenv
+;; Dockerfile mode
+(use-package dockerfile-mode
+  :ensure t
+  :config
+  (require 'dockerfile-mode)
+  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
+
+(use-package geiser
   :ensure t)
 
-(setq package-selected-packages 
-  '(dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company
-    ;; Optional packages
-    lsp-ui company hover))
+;; modern list API fir emacs-lisp
+(use-package dash
+  :ensure t)
 
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
+;; string manipulation API for emacs-lisp
+(use-package s
+  :ensure t)
 
-(add-hook 'dart-mode-hook 'lsp)
+(use-package f
+  :ensure t)
 
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      company-minimum-prefix-length 1
-      lsp-lens-enable t
-      lsp-signature-auto-activate nil)
+;; lisp utility
+(use-package paredit
+  :ensure t)
 
-(use-package lsp-dart
+;; Text manipulation and utilities
+;; org-mode stuff
+(use-package org-bullets
   :ensure t
-  :hook (dart-mode . lsp)
-  :init
-  (dap-register-debug-template "Flutter :: Custom debug"
-                               (list :flutterPlatform "x86_64"
-                                     :program "lib/main_debug.dart"
-                                     :args '("--flavor" "customer_a"))))
+  :config
+  (require 'org-bullets)
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+(use-package dart-mode
+  :ensure t)
 
-;; emacs.el ends here
+(use-package company
+  :ensure t)
+
+(use-package protobuf-mode
+  :ensure t)
+
+(use-package vue-mode
+  :mode "\\.vue\\'")
+
+(use-package kotlin-mode
+  :ensure t)
+
+(use-package eglot
+  :ensure t
+  :hook ((dart-mode . eglot-ensure))
+  :custom
+  (eglot-ignored-server-capabilities
+   '(:inlayHintProvider)))
+
+(use-package jenkins
+  :ensure t)
+
+;;; .emacs ends here
